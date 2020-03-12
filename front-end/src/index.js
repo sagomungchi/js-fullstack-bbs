cb5ce9639c9416f2c2f5b741cb7c979091484a97
SeanSin <seansin@chungbuk.ac.kr>
2020-03-12 18:22:09 +0900

    계정 인증 구현 후 폴더 정리

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//Router
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// Container Components
+import { App, Home, Login, Register } from './containers';
+// Redux 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <div>
+                <Route exact path="/" component={App} />
+                <Route path="/home" component={Home}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/register" component={Register}/>
+            </div>
+        </Router>
+    </Provider>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
