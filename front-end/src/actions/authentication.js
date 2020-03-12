80b384402ea4cc4398660190b02db7920f90d800
SeanSin <seansin@chungbuk.ac.kr>
2020-03-12 18:18:33 +0900

    redux action 추가 - 계정 인증(로그인/회원가입) 구현

---
+++ front-end/src/actions/authentication.js
@@ -0,0 +1,133 @@
+import axios from 'axios';
+import {
+    AUTH_LOGIN,
+    AUTH_LOGIN_SUCCESS,
+    AUTH_LOGIN_FAILURE,
+    AUTH_REGISTER,
+    AUTH_REGISTER_SUCCESS,
+    AUTH_REGISTER_FAILURE,
+    AUTH_GET_STATUS,
+    AUTH_GET_STATUS_SUCCESS,
+    AUTH_GET_STATUS_FAILURE,
+    AUTH_LOGOUT
+} from './ActionTypes';
+
+
+/*============================================================================
+    authentication
+==============================================================================*/
+
+/* LOGIN */
+export function loginRequest(username, password){
+    return (dispatch) => {
+        dispatch(login());
+        
+        return axios.post('http://localhost:5000/account/signin', { username, password })
+        .then((resp) => {
+            dispatch(loginSuccess(username));
+        }).catch((err) => {
+            dispatch(loginFailure());
+        });
+    };
+}
+
+export function login() {
+    return {
+        type: AUTH_LOGIN
+    };
+}
+
+export function loginSuccess(username) {
+    return {
+        type: AUTH_LOGIN_SUCCESS,
+        username
+    };
+}
+
+export function loginFailure() {
+    return {
+        type: AUTH_LOGIN_FAILURE
+    };
+}
+
+/* REGISTER */
+export function registerRequest(username, password) {
+    return (dispatch) => {
+        dispatch(register());
+        return axios.post('http://localhost:5000/account/signup', { username, password })
+        .then((response) => {
+            dispatch(registerSuccess());
+        }).catch((error) => {
+            dispatch(registerFailure(error.response.data.code));
+        });
+    };
+}
+
+export function register() {
+    return {
+        type: AUTH_REGISTER
+    };
+}
+
+export function registerSuccess() {
+    return {
+        type: AUTH_REGISTER_SUCCESS,
+    };
+}
+
+export function registerFailure(error) {
+    return {
+        type: AUTH_REGISTER_FAILURE,
+        error
+    };
+}
+
+/* GET STATUS */
+export function getStatusRequest() {
+    return (dispatch) => {
+        // inform Get Status API is starting
+        dispatch(getStatus());
+
+        return axios.get('/api/account/getInfo')
+        .then((response) => {
+            dispatch(getStatusSuccess(response.data.info.username));
+        }).catch((error) => {
+            dispatch(getStatusFailure());
+        });
+    };
+}
+
+export function getStatus() {
+    return {
+        type: AUTH_GET_STATUS
+    };
+}
+
+export function getStatusSuccess(username) {
+    return {
+        type: AUTH_GET_STATUS_SUCCESS,
+        username
+    };
+}
+
+export function getStatusFailure() {
+    return {
+        type: AUTH_GET_STATUS_FAILURE
+    };
+}
+
+/* Logout */
+export function logoutRequest() {
+    return (dispatch) => {
+        return axios.post('/api/account/logout')
+        .then((response) => {
+            dispatch(logout());
+        });
+    };
+}
+
+export function logout() {
+    return {
+        type: AUTH_LOGOUT
+    };
+}
