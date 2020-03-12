2c63208e1369603ca22984ed35a4ca93deea8836
SeanSin <seansin@chungbuk.ac.kr>
2020-03-12 13:40:02 +0900

    프론트엔드 초기 세팅본

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          I'M READY TO USE THE BACK END API
+        </p>
+    </div>
+  );
+}
+
+export default App;
