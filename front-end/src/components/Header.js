90b3442a881df4b9546f2dcb3676292040e26c28
SeanSin <seansin@chungbuk.ac.kr>
2020-03-12 18:18:59 +0900

    계정 인증(로그인 및 회원가입) 컴포넌트 구현

---
+++ front-end/src/components/Header.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class Header extends Component{
+    render(){
+        const loginButton = (
+            <li>
+                <a>
+                    <Link to="/login">
+                        <i className="material-icons">vpn_key</i>
+                    </Link>
+                </a>
+            </li>
+        );
+
+        const logoutButton = (
+            <li>
+                <a onClick={this.props.onLogout}>
+                    <i className="material-icons">lock_open</i>
+                </a>
+            </li>
+        );
+
+        return (
+            <nav>
+                <div className="nav-wrapper blue darken-1">
+                    <Link to="/" className="brand-logo center">MEMOPAD</Link>
+
+                    <ul>
+                        <li><a><i className="material-icons">search</i></a></li>
+                    </ul>
+
+                    <div className="right">
+                        <ul>
+                            { this.props.isLoggedIn ? logoutButton : loginButton }
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+Header.propTypes = {
+    isLoggedIn: PropTypes.bool,
+    onLogout: PropTypes.func
+};
+
+Header.defaultProps = {
+    isLoggedIn: false,
+    onLogout: () => { console.error("logout function not defined");}
+};
+
+export default Header;
